99549f21949bf603558098375e08fa2570a2cf8b
Jack4me <just4go@gmail.com>
2024-04-23 11:41:23 +0200

    +2

--- Assets/Scripts/Level/LevelData.cs
+++ Assets/Scripts/Level/LevelData.cs
@@ -1,13 +1,12 @@
 using System.Collections.Generic;
-using Level;
 using UnityEngine;
 
-namespace Configuration.LevelConfigurations.Data {
+namespace Level {
     [CreateAssetMenu(menuName = "DragonCraft/New Level")]
     public class LevelData : ScriptableObject {
         public List<LevelSlot> Slots = new ();
         public int Columns;
         public int Rows;
-        public LevelConfiguration Configuration;
+        public Configuration.LevelConfigurations.Data.LevelConfiguration Configuration;
     }
 }
