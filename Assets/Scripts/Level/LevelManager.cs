eeea154ad2fae35b7ce8d942d5b26d5ac981a9f5
Jack4me <just4go@gmail.com>
2024-04-24 11:48:31 +0200

    +1

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 namespace Level {
@@ -5,6 +6,9 @@ namespace Level {
     {
     
             [SerializeField] private GameObject _miniMapCameraPrefab;
+            [SerializeField] private GameObject _fog;
+            public static LevelManager Instance { private set; get; }
+            public List<GameObject> Units { private set; get; }
             private void Start()
             {
                 Instantiate(_miniMapCameraPrefab);
