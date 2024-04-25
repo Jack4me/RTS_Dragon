06f7824822aac0c90e9dd6316a6d09bfc0c9b023
Jack4me <just4go@gmail.com>
2024-04-25 10:05:45 +0200

    +1

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -1,20 +1,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 namespace Level {
-    public class LevelManager : MonoBehaviour
-    {
-    
-            [SerializeField] private GameObject _miniMapCameraPrefab;
-            [SerializeField] private GameObject _fog;
-            public static LevelManager Instance { private set; get; }
-            public List<GameObject> Units { private set; get; }
-            private void Start()
-            {
-                Instantiate(_miniMapCameraPrefab);
-                SceneManager.LoadScene("GameUI", LoadSceneMode.Additive);
+    public class LevelManager : MonoBehaviour {
+        [SerializeField] private GameObject _miniMapCameraPrefab;
+        [SerializeField] private GameObject _fog;
+        public static LevelManager Instance { private set; get; }
+        public List<GameObject> Units { private set; get; }
+
+        private void Awake() {
+            if (Instance != null && Instance != this) {
+                Destroy(this);
+                return;
             }
+
+            Instance = this;
+            Units = new List<GameObject>();
+        }
+
+        private void Start() {
+            Instantiate(_miniMapCameraPrefab);
+            SceneManager.LoadScene("GameUI", LoadSceneMode.Additive);
+            _fog.SetActive(true);
         }
     }
-    
-
+}
