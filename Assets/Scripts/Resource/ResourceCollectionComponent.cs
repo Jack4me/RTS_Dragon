876078cfa254d466a00afe841c0733b7ed093731
Jack4me <just4go@gmail.com>
2024-05-04 11:33:11 +0200

    +1

---
+++ Assets/Scripts/Resource/ResourceCollectionComponent.cs
@@ -0,0 +1,29 @@
+﻿using Configuration;
+using Unit;
+using UnityEngine;
+
+namespace Resource {
+    public class ResourceCollectionComponent : ResourceProductionComponent {
+        protected override void Start() {
+            base.Start();
+            _productionType = ResourceProductionType.Manual;
+        }
+        private void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.TryGetComponent<UnitComponent>(out UnitComponent unit))
+            {
+                if (unit.GetActionType() == ActionType.Collect)
+                {
+                    _productionType = ResourceProductionType.Automatic;
+                }
+            }
+        }
+        private void OnCollisionExit(Collision col)
+        {
+            if (col.gameObject.TryGetComponent<UnitComponent>(out var unit))
+            {
+                _productionType = ResourceProductionType.Manual;
+            }
+        }
+    }
+}
